7ba1571d7ea6d8a7b69a5c2b6c0d9cdc96046f04
ChristianKim <jongho.kim@rocket.is>
2021-07-30 15:33:19 +0900

    git init

---
+++ src/main/java/is/recruit/mycroft/spring/subjects/SpringSubjectsApplication.java
@@ -0,0 +1,13 @@
+package is.recruit.mycroft.spring.subjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSubjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSubjectsApplication.class, args);
+	}
+
+}
